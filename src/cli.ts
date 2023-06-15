06064de2f1955afe8ea443f75376de41a6e31043
Adrian Machado <amachado@uwaterloo.ca>
2023-06-15 12:25:24 -0700

    Add spectral #1

--- src/cli.ts
+++ src/cli.ts
@@ -3,11 +3,13 @@ dotenv.config();
 import { hideBin } from "yargs/helpers";
 import yargs from "yargs/yargs";
 import rate from "./cmds/generate-rating.js";
+import rateSpectral from "./cmds/generate-spectral-rating.js";
 
 void yargs(hideBin(process.argv))
   // This means that all env vars will have to be prefixed with RATE_MY_OPEN_API_
   .env("RATE_MY_OPEN_API")
   .command(rate)
+  .command(rateSpectral)
   .demandCommand()
   .strictCommands()
   .version()

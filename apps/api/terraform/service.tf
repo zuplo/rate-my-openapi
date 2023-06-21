8592839df8fa373efcf8f302721610ed0e4c8fa4
Nathan Totten <nathan@ntotten.com>
2023-06-21 17:48:15 -0400

    Added api domain

--- apps/api/terraform/service.tf
+++ apps/api/terraform/service.tf
@@ -78,3 +78,12 @@ resource "google_cloud_run_v2_service_iam_binding" "default" {
     "allUsers"
   ]
 }
+
+resource "google_cloud_run_domain_mapping" "default" {
+  location = var.region
+  name     = "api.ratemyopenapi.com"
+
+  spec {
+    route_name = google_cloud_run_v2_service.default.name
+  }
+}

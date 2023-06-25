840930d86b90cab3a410e602e2197342348aac05
Jon Hutchison <jon.hutchison@leafly.com>
2023-06-25 13:01:26 -0500

    Initial home and reports layouts, updated configs (#8)

---
+++ apps/web/src/app/report/[id]/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Report - Rate My OpenAPI",
+};
+
+const ReportLayout = ({ children }: { children: React.ReactNode }) => {
+  return <main className="my-8">{children}</main>;
+};
+
+export default ReportLayout;

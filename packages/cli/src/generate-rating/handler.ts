99d1df3d26b01ebbaf2a88ded385e3b11d0e3acc
Adrian Machado <amachado@uwaterloo.ca>
2023-07-05 15:06:30 -0700

    SDK generation rating #25

--- packages/cli/src/generate-rating/handler.ts
+++ packages/cli/src/generate-rating/handler.ts
@@ -1,10 +1,22 @@
+import {
+  RatingOutput,
+  SpectralReport,
+  generateOpenApiRating,
+} from "@rate-my-openapi/core";
+import spectralCore from "@stoplight/spectral-core";
+import Parsers from "@stoplight/spectral-parsers"; // make sure to install the package if you intend to use default parsers!
+import { bundleAndLoadRuleset } from "@stoplight/spectral-ruleset-bundler/with-loader";
+import spectralRuntime from "@stoplight/spectral-runtime";
 import { exec } from "child_process";
+import * as fs from "node:fs";
 import { readFile } from "node:fs/promises";
 import { join, relative } from "node:path";
 import util from "node:util";
 import { OpenAPIV3, OpenAPIV3_1 } from "openapi-types";
 import { printCriticalFailureToConsoleAndExit } from "../common/output.js";
-import { RatingOutput, SpectralReport, generateOpenApiRating } from "@rate-my-openapi/core";
+
+const { Spectral, Document } = spectralCore;
+const { fetch } = spectralRuntime;
 const execAwait = util.promisify(exec);
 export interface Arguments {
   filepath: string;
@@ -16,18 +28,19 @@ export async function generateRating(argv: Arguments) {
     const filepath = argv.filepath;
     const pathName = join(relative(process.cwd(), filepath));
     const rulesetPath = join(
-      process.cwd(), "../", "../", "rulesets/rules.vacuum.yaml",
+      process.cwd(),
+      "../",
+      "../",
+      "rulesets/rules.vacuum.yaml"
     );
-    const openApiFile = await readFile(
-      pathName,
-    );
+    const openApiFile = await readFile(pathName);
     const openApi = JSON.parse(openApiFile.toString()) as
       | OpenAPIV3_1.Document
       | OpenAPIV3.Document;
     const startTime = Date.now();
     const { stdout, stderr } = await execAwait(
       `vacuum spectral-report -r ${rulesetPath} -o ${pathName}`,
-      { maxBuffer: undefined },
+      { maxBuffer: undefined }
     );
 
     if (stderr) {
@@ -42,6 +55,35 @@ export async function generateRating(argv: Arguments) {
     } catch (err) {
       throw new Error(`Failed to parse vacuum output to JSON: ${err}`);
     }
+
+    // Spectral supplement
+    // There are some issues with Vacuum that need to be resolved before we can
+    // solely use it. These spectral rules cover the gaps
+    // Issues:
+    // - https://github.com/daveshanley/vacuum/issues/303
+    // - https://github.com/daveshanley/vacuum/issues/283
+    // - https://github.com/daveshanley/vacuum/issues/269
+    const openApiSpectralDoc = new Document(
+      openApiFile.toString(),
+      Parsers.Json,
+      filepath
+    );
+    const spectral = new Spectral();
+    const rulesetFilepath = join(
+      process.cwd(),
+      "../",
+      "../",
+      "rulesets",
+      ".spectral-supplement.yaml"
+    );
+    spectral.setRuleset(
+      await bundleAndLoadRuleset(rulesetFilepath, { fs, fetch })
+    );
+    const spectralOutputReport: SpectralReport = await spectral.run(
+      openApiSpectralDoc
+    );
+    outputReport = [...outputReport, ...spectralOutputReport];
+
     const endTime = Date.now();
     const output: RatingOutput = generateOpenApiRating(outputReport, openApi);
     console.log(JSON.stringify(output, null, 2));
@@ -51,6 +93,7 @@ export async function generateRating(argv: Arguments) {
     // console.log("Time to run operation: ", opFinishTime - opStartTime, "ms");
     process.exit(0);
   } catch (err) {
+    console.log(err);
     printCriticalFailureToConsoleAndExit(err);
   }
 }

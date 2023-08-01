bfe6cf9a33c3bec82a1c08604d6a06380cfc64a1
Abdallah Abedraba <aabedraba@gmail.com>
2023-08-02 01:36:42 +0400

    Update iam.tf

--- apps/api/terraform/iam.tf
+++ apps/api/terraform/iam.tf
@@ -2,8 +2,8 @@ resource "google_service_account" "default" {
   account_id = "rate-my-open-api"
 }
 
-# resource "google_project_iam_member" "artifactregistry_read_dev_api_sa" {
-#   project = var.project_id
-#   role    = "roles/artifactregistry.reader"
-#   member  = "serviceAccount:${google_service_account.default.email}"
-# }
+resource "google_project_iam_member" "storage_admin_sa" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.default.email}"
+}

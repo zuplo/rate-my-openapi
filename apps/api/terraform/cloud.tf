5907dc13fc39b9a23a897dc9f5bd1808f38033b3
Nathan Totten <nathan@ntotten.com>
2023-08-01 17:15:00 -0400

    Added terraform

---
+++ apps/api/terraform/cloud.tf
@@ -0,0 +1,8 @@
+terraform {
+  cloud {
+    organization = "zuplo"
+    workspaces {
+      name = "rate-my-open-api"
+    }
+  }
+}
---
+++ apps/api/terraform/iam.tf
@@ -0,0 +1,9 @@
+resource "google_service_account" "default" {
+  account_id = "rate-my-open-api"
+}
+
+# resource "google_project_iam_member" "artifactregistry_read_dev_api_sa" {
+#   project = var.project_id
+#   role    = "roles/artifactregistry.reader"
+#   member  = "serviceAccount:${google_service_account.default.email}"
+# }

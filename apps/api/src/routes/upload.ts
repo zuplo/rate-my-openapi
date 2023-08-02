7013faff17e7a8bf149bedd74158c91f18aa0d8a
aabedraba <aabedraba@gmail.com>
2023-08-02 15:28:42 +0400

    simplify upload

--- apps/api/src/routes/upload.ts
+++ apps/api/src/routes/upload.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+import fastifyMultipart from "@fastify/multipart";
 import { type FastifyPluginAsync } from "fastify";
+import { inngestInstance } from "src/services/inngest.js";
 import { v4 as uuidv4 } from "uuid";
-import { getUploadSignedUrl } from "../services/storage.js";
+import { getStorageBucketName, storage } from "../services/storage.js";
 
 const uploadRoute: FastifyPluginAsync = async function (server) {
   server.route({
@@ -19,59 +20,70 @@ const uploadRoute: FastifyPluginAsync = async function (server) {
     url: "/upload",
     handler: async (request, reply) => {
       const parts = request.parts();
+      const parseResult = await parseMultipartUpload(parts);
 
-      let file;
-      let email;
-      let fileExt;
-      for await (const part of parts) {
-        if (part.type === "file" && part.fieldname === "apiFile") {
-          file = await part.toBuffer();
-          const nonValidatedFileExtension = part.filename.split(".").pop();
-          fileExt = validateFileExtension(nonValidatedFileExtension);
-        } else if (part.type === "field" && part.fieldname === "emailAddress") {
-          email = part.value;
-        }
-      }
-
-      if (!file || !email || !fileExt) {
+      if (!parseResult) {
         return reply
           .code(400)
           .header("Content-Type", "application/json; charset=utf-8")
-          .send({ error: "Request missing required fields" });
+          .send({ message: "Invalid request body" });
       }
 
-      const uuid = uuidv4();
-      const signedUrl = await getUploadSignedUrl(`${uuid}.${fileExt}`);
-
-      let fileUploadSuccess = false;
       try {
-        const { status } = await axios.put(signedUrl, file, {
-          headers: {
-            "Content-Type": "application/octet-stream",
+        const uuid = uuidv4();
+        const fileContents = Buffer.from(parseResult.file).toString();
+        const fileName = `${uuid}.${parseResult.fileExtension}`;
+
+        await storage
+          .bucket(getStorageBucketName())
+          .file(fileName)
+          .save(fileContents);
+
+        await inngestInstance.send({
+          name: "api/file.uploaded",
+          data: {
+            id: uuid,
+            email: parseResult.email,
+            fileExtension: parseResult.fileExtension,
           },
         });
 
-        if (status === 200) {
-          fileUploadSuccess = true;
-        }
-      } catch (e) {
-        throw new Error(e.response.data);
-      }
-
-      if (!fileUploadSuccess) {
-        server.log.error(`File upload failed for ${uuid}`);
         return reply
-          .code(500)
+          .code(200)
           .header("Content-Type", "application/json; charset=utf-8")
-          .send({ error: "File upload failed" });
+          .send({ id: uuid });
+      } catch (e) {
+        throw new Error(e);
       }
-
-      return reply
-        .code(200)
-        .header("Content-Type", "application/json; charset=utf-8")
-        .send({ id: uuid });
     },
   });
+};
+
+const parseMultipartUpload = async (
+  parts: AsyncIterableIterator<fastifyMultipart.Multipart>
+) => {
+  let file;
+  let email;
+  let fileExtension;
+  for await (const part of parts) {
+    if (part.type === "file" && part.fieldname === "apiFile") {
+      file = await part.toBuffer();
+      const nonValidatedFileExtension = part.filename.split(".").pop();
+      fileExtension = validateFileExtension(nonValidatedFileExtension);
+    } else if (part.type === "field" && part.fieldname === "emailAddress") {
+      email = part.value;
+    }
+  }
+
+  if (!file || !email || !fileExtension) {
+    return null;
+  }
+
+  return {
+    file,
+    email,
+    fileExtension,
+  };
 };
 
 const validateFileExtension = (fileExtension: string | undefined) => {

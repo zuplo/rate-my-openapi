8c86529d1b0f109b842d8a0bf042eac1cd2271bb
Abdallah Abedraba <aabedraba@gmail.com>
2023-08-02 19:46:41 +0400

    run CI from root and add sendgrid (#75)
    * update fly.toml
    
    * CI from root
    
    * add sendgrid

---
+++ apps/api/src/lib/email.ts
@@ -0,0 +1,42 @@
+import sgMail from "@sendgrid/mail";
+import esMain from "es-main";
+sgMail.setApiKey(process.env.SENDGRID_API_KEY || "");
+
+export const sendReportEmail = async ({
+  email,
+  reportId,
+}: {
+  email: string;
+  reportId: string;
+}) => {
+  const msg = {
+    to: email,
+    from: "hello@ratemyopenapi.com",
+    subject: "Your OpenAPI Report is Ready",
+    text: "Visit here: https://ratemyopenapi.com/report/" + reportId,
+    html:
+      "Visit your report here: https://ratemyopenapi.com/report/" + reportId,
+  };
+
+  try {
+    const emailSend = await sgMail.send(msg);
+
+    return {
+      statusCode: emailSend[0].statusCode,
+      headers: emailSend[0].headers,
+    };
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+// Run this without needing to run the entire app
+// `node dist/services/sendgrid.js <email> <reportId>`
+if (esMain(import.meta))
+  sendReportEmail({
+    email: process.argv[2],
+    reportId: process.argv[3],
+  })
+    .then(console.log)
+    .catch(console.error);

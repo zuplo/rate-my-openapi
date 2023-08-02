7a8cd05d39f3bc04eb08a48da503d4c08cc4f2c1
aabedraba <aabedraba@gmail.com>
2023-08-02 17:19:19 +0400

    fix health endpoint in docker

--- apps/api/src/routes/health.ts
+++ apps/api/src/routes/health.ts
@@ -24,7 +24,7 @@ const healthRoute: FastifyPluginAsync = async function (server) {
 
       try {
         const pkgJson = await fs.readFile(
-          path.resolve(process.cwd(), "../../package.json"),
+          path.resolve(process.cwd(), "./package.json"),
           "utf-8"
         );
         pkg = JSON.parse(pkgJson);

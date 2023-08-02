484cabfa4135d0d766eab2b9cfffd8b69f34f93d
Nathan Totten <nathan@ntotten.com>
2023-08-02 10:29:47 -0400

    updated storage iam

--- apps/api/terraform/iam.tf
+++ apps/api/terraform/iam.tf
@@ -2,9 +2,3 @@ resource "google_service_account" "default" {
   display_name = "Rate My Open API (production)"
   account_id   = "rate-my-open-api"
 }
-
-resource "google_project_iam_member" "storage_admin_sa" {
-  project = var.project_id
-  role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.default.email}"
-}
--- apps/api/terraform/storage.tf
+++ apps/api/terraform/storage.tf
@@ -2,3 +2,21 @@ resource "google_storage_bucket" "default" {
   name     = "api-ratemyopeanpi-com-prod"
   location = "US"
 }
+
+# See https://cloud.google.com/storage/docs/access-control/iam-roles
+# Revisit at https://linear.app/zuplo/issue/ZUP-294/revisit-permissions-for-storage-service-account
+resource "google_storage_bucket_iam_binding" "default_object_writer" {
+  bucket = google_storage_bucket.default.name
+  role   = "roles/storage.objectCreator"
+  members = [
+    "serviceAccount:${google_service_account.defaykt.email}"
+  ]
+}
+
+resource "google_storage_bucket_iam_binding" "default_object_viewer" {
+  bucket = google_storage_bucket.default.name
+  role   = "roles/storage.objectViewer"
+  members = [
+    "serviceAccount:${google_service_account.defaykt.email}"
+  ]
+}

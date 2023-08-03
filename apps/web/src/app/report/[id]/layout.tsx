a4bc28396c23db7b967b5507dba01a6dce17b7bc
aabedraba <aabedraba@gmail.com>
2023-08-03 16:35:13 +0400

    add og image generation

--- apps/web/src/app/report/[id]/layout.tsx
+++ apps/web/src/app/report/[id]/layout.tsx
@@ -1,6 +1,22 @@
-export const metadata = {
-  title: "Report - Rate My OpenAPI",
+import { Metadata } from "next";
+
+type Props = {
+  params: { id: string };
 };
+
+export function generateMetadata({ params }: Props): Metadata {
+  const id = params.id;
+  return {
+    title: "Report - Rate My OpenAPI",
+    openGraph: {
+      images: [
+        {
+          url: `https://ratemyopenapi.com/api/og/${id}`,
+        },
+      ],
+    },
+  };
+}
 
 const ReportLayout = ({ children }: { children: React.ReactNode }) => (
   <main className="pb-8 pt-[90px] md:pt-[136px]">{children}</main>

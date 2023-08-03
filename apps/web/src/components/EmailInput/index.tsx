d1776d08542349fbcf354f3ee6f107a3add6729a
aabedraba <aabedraba@gmail.com>
2023-08-04 02:09:47 +0400

    handle upload error

--- apps/web/src/components/EmailInput/index.tsx
+++ apps/web/src/components/EmailInput/index.tsx
@@ -35,11 +35,15 @@ const EmailInput = () => {
         formData.append("emailAddress", emailInput?.value);
         formData.append("apiFile", file);
 
-        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/upload`, {
+        const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/upload`, {
           method: "POST",
           body: formData,
           mode: "no-cors",
         });
+
+        if (!req.ok) {
+          throw new Error("Could not upload file.");
+        }
 
         setNextStep();
       } catch (e) {

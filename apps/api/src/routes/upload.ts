04b1cc0e50d6a2e6e3d4dee82bd046d19761c077
Adrian Machado <adrian@zuplo.com>
2023-10-05 10:06:36 -0700

    Restrict to OpenAPI 3.x (#189)
    * Restrict to OpenAPI 3.x
    
    * Add missing version

--- apps/api/src/routes/upload.ts
+++ apps/api/src/routes/upload.ts
@@ -105,6 +105,20 @@ const parseMultipartUpload = async (
     });
   }
   const fileContentString = fileContent.toString();
+  if (
+    !fileContentString.includes("3.0.0") ||
+    !fileContentString.includes("3.0.1") ||
+    !fileContentString.includes("3.0.2") ||
+    !fileContentString.includes("3.0.3") ||
+    !fileContentString.includes("3.1.0")
+  ) {
+    return Err({
+      userMessage: "Invalid OpenAPI version. Only OpenAPI v3.x is supported",
+      debugMessage: "Invalid OpenAPI version",
+      statusCode: 400,
+    });
+  }
+
   const fileIsJsonOrYamlResult = checkFileIsJsonOrYaml(fileContentString);
 
   if (fileIsJsonOrYamlResult.err) {

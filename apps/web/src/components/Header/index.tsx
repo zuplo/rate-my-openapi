97155a54e1420afdf8a04e16c32f05cbfe28c0da
aabedraba <aabedraba@gmail.com>
2023-10-10 13:43:09 +0400

    change link to a tag

--- apps/web/src/components/Header/index.tsx
+++ apps/web/src/components/Header/index.tsx
@@ -5,11 +5,11 @@ const Header = () => (
   <header className="px-4 pt-4 md:pt-8">
     <div className="flex justify-between rounded-xl bg-white p-5 shadow-md">
       <div className="relative">
-        <Link href="/" className="flex justify-center">
+        <a href="/" className="flex justify-center">
           <h1 className=" text-2xl font-bold">
             rate<span className="text-gray-500">my</span>openapi
           </h1>
-        </Link>
+        </a>
         <div className="bg-zuplo-pink font-semibold absolute left-36 top-6 -rotate-[24deg] rounded-full bg-pink-500 px-2 text-xs text-white shadow-md drop-shadow-sm">
           Beta
         </div>

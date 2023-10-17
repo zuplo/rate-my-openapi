5e9963f5022d1f183a2623d6675792613b0b1f3c
aabedraba <aabedraba@gmail.com>
2023-10-18 00:25:10 +0400

    fix mobile layout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,8 +6,8 @@ import UploadInterface from "@/components/UploadInterface";
 import { UploadContextProvider } from "@/contexts/UploadContext";
 
 const HomePage = () => (
-  <div className="flex flex-col items-center justify-center md:mt-10">
-    <h2 className="mb-12 max-w-3xl text-center text-5xl font-bold md:mb-16 md:text-7xl">
+  <div className="mx-4 flex flex-col items-center justify-center md:mx-0 md:mt-10">
+    <h2 className="mb-12 mt-4 max-w-3xl text-center text-5xl font-bold md:mb-16 md:mt-0 md:text-7xl">
       Upload your OpenAPI.
       <br />
       We rate it.
--- apps/web/src/components/Header/index.tsx
+++ apps/web/src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import LogoIcon from "../LogoIcon";
 
 const Header = () => (

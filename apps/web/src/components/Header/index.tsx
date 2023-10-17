9682b0fa5d4ff73028b5ec951536c50d085f3be6
Josh Twist <joshtwist@users.noreply.github.com>
2023-10-17 09:51:42 +0200

    Remove (beta)
    removing

--- apps/web/src/components/Header/index.tsx
+++ apps/web/src/components/Header/index.tsx
@@ -10,9 +10,6 @@ const Header = () => (
             rate<span className="text-gray-500">my</span>openapi
           </h1>
         </a>
-        <div className="bg-zuplo-pink font-semibold absolute left-36 top-6 -rotate-[24deg] rounded-full bg-pink-500 px-2 text-xs text-white shadow-md drop-shadow-sm">
-          Beta
-        </div>
       </div>
       <a href="https://zuplo.com">
         <LogoIcon height={33} width={33} />

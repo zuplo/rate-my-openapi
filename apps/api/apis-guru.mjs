d6250a6db84dba717a4e077116aa15912fbd1f23
Nathan Totten <nathan@ntotten.com>
2023-11-13 20:08:41 -0500

    updated script

--- apps/api/apis-guru.mjs
+++ apps/api/apis-guru.mjs
@@ -24,7 +24,7 @@ const response = await fetch(
 );
 let ratings = await response.json();
 
-await queue.addAll(files[0].map((file) => () => rateFile(file)));
+await queue.addAll(files.map((file) => () => rateFile(file)));
 
 const reportJson = JSON.stringify(ratings, null, 2);
 await fs.promises.writeFile(ratingsPath, reportJson, "utf-8");

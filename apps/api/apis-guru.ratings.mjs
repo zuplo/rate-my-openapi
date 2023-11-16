e840de9553a5a185f280ff46b3400a4855293e08
Nathan Totten <nathan@ntotten.com>
2023-11-16 14:12:36 -0500

    updated ratings script

--- apps/api/apis-guru.ratings.mjs
+++ apps/api/apis-guru.ratings.mjs
@@ -21,18 +21,24 @@ for (const line of logLines.split("\n")) {
   }
 }
 
-const outputRatings = ratings
-  .map((r) => {
-    const updatedRating = updatedRatings.find((ur) => ur.file === r.file);
-    if (updatedRating) {
-      return updatedRating;
-    }
-    return r;
-  })
-  .filter((r) => r !== undefined);
+updatedRatings.forEach((r) => {
+  const oldRating = ratings.find(
+    (ur) => ur.name === r.name && ur.version === r.version,
+  );
+  if (oldRating) {
+    oldRating.score = r.score;
+    oldRating.reportId = r.reportId;
+    oldRating.hash = r.hash;
+  } else {
+    ratings.push(r);
+  }
+});
+
+console.log(`Found ${updatedRatings.length} updated ratings`);
+console.log(`Total ratings after merge: ${ratings.length}`);
 
 // Just to make sure we create fully valid json without undefined values
-const response = new Response(JSON.stringify(outputRatings));
+const response = new Response(JSON.stringify(ratings));
 const result = await response.json();
 const reportJson = JSON.stringify(result, null, 2);
 await fs.promises.writeFile(ratingsPath, reportJson, "utf-8");

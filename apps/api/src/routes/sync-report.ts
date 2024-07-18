f40037e7ac5ce5c407568f538e1ae8a4a1e39f30
Luis Campos Guajardo <luis@zuplo.com>
2024-07-18 14:21:49 +0200

    clean up for prod use

--- apps/api/src/routes/sync-report.ts
+++ apps/api/src/routes/sync-report.ts
@@ -9,35 +9,21 @@ import validateOpenapi, {
 } from "../lib/validate-openapi.js";
 import { postSlackMessage } from "../services/slack.js";
 import { getStorageBucketName, getStorageClient } from "../services/storage.js";
-import { generateRatingFromStorage, getReport } from "../lib/rating.js";
-import { tmpdir } from "node:os";
-import path from "node:path";
-import fs from "node:fs";
+import { generateRatingFromStorage } from "../lib/rating.js";
 
 const syncReportRoute: FastifyPluginAsync = async function (server) {
   server.route({
     method: "POST",
-    /* schema: {
-      response: {
-        200: {
-          type: "object",
-          properties: {
-            id: { type: "string" },
-          },
-        },
-      },
-    },*/
+    // @TODO - will add a schema after agreeing on final format
     url: "/sync-report",
     handler: async (request, reply) => {
-      console.log("request received in sync-report");
       const parts = request.parts();
       let parseResult: ParseMultipartUploadResult;
       try {
         parseResult = await parseMultipartUpload(parts);
-        console.log("after parsing");
       } catch (err) {
         Sentry.captureException(err);
-        /* await postSlackMessage({
+        await postSlackMessage({
           text: `Failed to parse uploaded file with error: ${
             err.detail ?? err.message
           }. Request ID: ${request.id}. ${
@@ -45,18 +31,18 @@ const syncReportRoute: FastifyPluginAsync = async function (server) {
               ? `API User: ${request.headers["api-user"]}`
               : ""
           }}`,
-        });*/
+        });
         throw err;
       }
 
       // If not an API key user, error out
-      /*      if (!request.headers["api-user"]) {
+      if (!request.headers["api-user"]) {
         throw new ApiError({
-          ...Problems.BAD_REQUEST,
-          detail: "Invalid request body. No api key provided",
+          ...Problems.FORBIDDEN,
+          detail: "Please provide an api key.",
         });
       }
-*/
+
       const reportId = randomUUID();
       const { fileContentString: content, fileExtension } = parseResult;
 
@@ -71,20 +57,6 @@ const syncReportRoute: FastifyPluginAsync = async function (server) {
 
       const fileName = `${reportId}.${fileExtension}`;
 
-      // local runs
-      const tempPath = path.join(tmpdir(), fileName);
-
-      console.log(`storing file in ${tempPath}`);
-      fs.writeFileSync(tempPath, content);
-
-      const results = await getReport({
-        fileContent: content,
-        fileExtension: fileExtension,
-        reportId,
-        openAPIFilePath: tempPath,
-      });
-
-      /*
       await getStorageClient()
         .bucket(getStorageBucketName())
         .file(fileName)
@@ -94,7 +66,6 @@ const syncReportRoute: FastifyPluginAsync = async function (server) {
         reportId,
         fileExtension,
       });
-*/
 
       return reply.code(200).send({
         results,
@@ -116,12 +87,10 @@ export async function parseMultipartUpload(
   let fileContent;
   for await (const part of parts) {
     if (part.type === "file") {
-      console.log("file to buffer");
       fileContent = await part.toBuffer();
     }
   }
 
-  console.log("fileContent ====");
   if (!fileContent) {
     throw new ApiError({
       ...Problems.BAD_REQUEST,
@@ -130,7 +99,6 @@ export async function parseMultipartUpload(
   }
   const fileContentString = fileContent.toString();
 
-  console.log(`fileContentString ==> ${fileContentString}`);
   const fileIsJsonOrYamlResult = checkFileIsJsonOrYaml(fileContentString);
   if (typeof fileIsJsonOrYamlResult !== "string") {
     throw new ApiError({
@@ -139,7 +107,6 @@ export async function parseMultipartUpload(
     });
   }
 
-  console.log("validate openapi file content");
   const openApiValidationResult = validateOpenapi({
     fileContent: fileContentString,
     fileExtension: fileIsJsonOrYamlResult,

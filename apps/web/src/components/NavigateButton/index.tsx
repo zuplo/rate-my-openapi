adf44c009c8ada6f38661bc387e51019ac12035c
Luis Campos Guajardo <luis@zuplo.com>
2024-07-31 10:04:48 -0700

    Adjustments so everything fits nicely

--- apps/web/src/components/NavigateButton/index.tsx
+++ apps/web/src/components/NavigateButton/index.tsx
@@ -4,7 +4,7 @@ const NavigateButton = ({ label, url }: { label: string; url: string }) => {
   return (
     <button
       type="button"
-      className="mt-auto block rounded-md bg-fuchsia-600 p-2 font-medium text-white transition-colors hover:bg-gray-900 hover:text-white"
+      className="mt-auto block rounded-md bg-fuchsia-600 p-2 font-medium text-white transition-colors hover:bg-fuchsia-500 hover:text-white"
       onClick={() => {
         window.location.href = url;
       }}

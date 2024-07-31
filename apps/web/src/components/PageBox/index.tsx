1d2fecf09cb9a67b1ea795e5653a06a9ac6818e4
Luis Campos Guajardo <luis@zuplo.com>
2024-07-30 18:57:42 -0700

    Update tool boxes to render on small screens, align code sample, nav button, update copy

--- apps/web/src/components/PageBox/index.tsx
+++ apps/web/src/components/PageBox/index.tsx
@@ -11,7 +11,7 @@ export const PageBox = ({
   <div
     className={classNames(
       "relative flex flex-col gap-4 rounded-lg bg-white p-8 shadow-md",
-      "after:absolute after:-right-1.5 after:-top-1.5 after:rounded-md after:bg-green-500 after:px-2 after:py-1.5 after:text-xs after:font-medium after:uppercase after:text-white after:content-['New']",
+      "after:absolute after:-right-1.5 after:-top-1.5 after:rounded-md after:bg-indigo-700 after:px-3 after:py-2 after:text-xs after:font-medium after:uppercase after:text-white after:content-['New']",
       className,
     )}
   >
